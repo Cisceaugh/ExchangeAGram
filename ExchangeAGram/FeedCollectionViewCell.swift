07b48dbed0f186425f1c1f3b0090524afd7f4a9b
Cisco <franciscoraglandjr@gmail.com>
2016-04-13 19:09:12 -0700

    first commit

---
+++ ExchangeAGram/FeedCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedCollectionViewCell.swift
+//  ExchangeAGram
+//
+//  Created by Francisco Ragland Jr on 4/13/16.
+//  Copyright © 2016 Francisco Ragland. All rights reserved.
+//
+
+import UIKit
+
+class FeedCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  ExchangeAGram
+//
+//  Created by Francisco Ragland Jr on 4/13/16.
+//  Copyright © 2016 Francisco Ragland. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class FeedItem: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
